cb5539a189b9beaaaa360efb4036fea048cd678b
unknown <rido@gmail.com>
2025-01-29 18:59:35 +0700

    fixing token handler

--- middleware/tokenHandler.js
+++ middleware/tokenHandler.js
@@ -3,18 +3,20 @@ const jwt = require("jsonwebtoken");
 
 const tokenHandler = asyncHandler(async (req, res, next) => {
     let token;
-    let authHeader = req.headers.authorization || req.headers.Authorization;
-    if (authHeader && authHeader.startsWith("Bearer")) {
-        token = authHeader.split(" ")[1];
-        jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
-            if (err) {
-                res.status(401);
-                throw new Error("Not authorized, token failed");
-            }
-            req.user = decoded;
-            next();
-        });
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        res.status(401);
+        throw new Error("Not authorized, no token provided");
     }
+
+    token = authHeader.split(" ")[1];
+
+    // Verify the token (If verification fails, it will automatically throw an error)
+    const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+
+    req.user = decoded; // Attach decoded user data to request
+    next();
 });
 
 module.exports = tokenHandler;
